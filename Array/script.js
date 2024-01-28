25a3c4f69e76a74a166979838e950fa59a1c4b6b
Md Rayhan Ahmed <rayhanahmed641@gmail.com>
2024-01-28 11:22:13 +0600

    Initial commit

---
+++ Array/script.js
@@ -0,0 +1,15 @@
+
+//splice
+
+// let arr = [1, 2, 3, 4, 5, 6, 7];
+// arr.splice(2, 2, 9, 10);
+// console.log(arr);
+
+
+// Practice Problem
+
+let arr = ["Bloomberg", "Microsoft", "Uber", "Google", "IBM", "Netflix"];
+arr.shift(); //a
+arr.splice(1, 1, "Ola");
+arr.push("Amazon");
+console.log(arr);
