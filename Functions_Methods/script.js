ff2fee7087d07eade3751ea3149fc0754ca6680a
Md Rayhan Ahmed <rayhanahmed641@gmail.com>
2024-09-16 22:09:13 +0600

    update function

--- Functions_Methods/script.js
+++ Functions_Methods/script.js
@@ -29,3 +29,23 @@ const countvowels = (str) => {
 }
 let res = countvowels("iou");
 console.log(res)
+
+//-------------------------------------------
+// Hackerrank Problem-Basic
+
+'use strict'
+function fizzBuzz(number) {
+    for (let i = 1; i <= number; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log('FizzBuzz');
+        } else if (i % 3 === 0) {
+            console.log('Fizz');
+        } else if (i % 5 === 0) {
+            console.log('Buzz');
+        } else {
+            console.log(i);
+        }
+    }
+}
+
+fizzBuzz(10);
