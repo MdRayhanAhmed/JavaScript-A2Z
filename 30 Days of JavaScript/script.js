71087b1eb6fcd10b609700a96c3de498eb91fe22
Md Rayhan Ahmed <rayhanahmed641@gmail.com>
2024-09-22 22:42:04 +0600

    Update script.js

--- 30 Days of JavaScript/script.js
+++ 30 Days of JavaScript/script.js
@@ -23,3 +23,19 @@ const result = (function (a, b) {
     return sum;
 })(3, 4);
 console.log(result); // 7
+
+// Functions Within Functions
+/* A powerful feature of JavaScript is you can actually create functions
+    within other functions and even return them!
+*/
+function createFunction() {
+    function f(a, b) {
+        const sum = a + b;
+        return sum;
+    }
+    return f;
+}
+const f = createFunction();
+console.log(f(3, 4)); // 7
+//In this example, createFunction() returns a new function. Then that function
+// can be used as normal.
