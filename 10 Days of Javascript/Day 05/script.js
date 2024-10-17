d919b1bef9cc0e6474760bb983c94142db6dbf3b
Md Rayhan Ahmed <rayhanahmed641@gmail.com>
2024-10-17 23:13:31 +0600

    add day5 pblm-16,17 solution

--- 10 Days of Javascript/Day 05/script.js
+++ 10 Days of Javascript/Day 05/script.js
@@ -21,41 +21,42 @@ class Square extends Rectangle {
 
 // Problem 16 - Day 5: Template Literals
 
-function getLetter(s) {
-    let letter;
-    // Write your code here
-    switch (s[0]) {
-        case 'a' || 'e' || 'i' || 'o' || 'u':
-            letter = 'A';
-            break;
-        case 'b' || 'c' || 'd' || 'f' || 'g':
-            letter = 'B';
-            break;
-        case 'h' || 'j' || 'k' || 'l' || 'm':
-            letter = 'C';
-            break;
-        default:
-            letter = 'D';
-    }
 
-    return letter;
+/*
+ * Determine the original side lengths and return an array:
+ * - The first element is the length of the shorter side
+ * - The second element is the length of the longer side
+ * 
+ * Parameter(s):
+ * literals: The tagged template literal's array of strings.
+ * expressions: The tagged template literal's array of expression values (i.e., [area, perimeter]).
+ */
+function sides(literals, ...expressions) {
+    const A = expressions[0];
+    const P = expressions[1];
+    const s1 = (P + (Math.sqrt(P * P - 16 * A))) / 4;
+    const s2 = (P - (Math.sqrt(P * P - 16 * A))) / 4;
+    const total = [s1, s2].sort((a, b) => {
+        return a - b;
+    })
+    return total;
 }
 
-console.log(getLetter('adfgt'))
+
+
 
 // Problem 17 - Day 5: Arrow Functions
 
-function vowelsAndConsonants(s) {
-    for (let i = 0; i < s.length; i++) {
-        if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u') {
-            console.log(s[i]);
-        }
-    }
-    for (let i = 0; i < s.length; i++) {
-        if (s[i] !== 'a' && s[i] !== 'e' && s[i] !== 'i' && s[i] !== 'o' && s[i] !== 'u') {
-            console.log(s[i]);
-        }
-    }
+
+/*
+ * Modify and return the array so that all even elements are doubled and all odd elements are tripled.
+ * 
+ * Parameter(s):
+ * nums: An array of numbers.
+ */
+function modifyArray(nums) {
+    //return nums.map (s => s%2!=0? s*3: s*2)
+    return nums.map((value) => value % 2 != 0 ? value * 3 : value * 2)
 }
 
-vowelsAndConsonants('javascriptloops')
+
