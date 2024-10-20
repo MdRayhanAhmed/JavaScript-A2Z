ed2a5ceabaef6d861badef1232f79b16e59bdbce
Md Rayhan Ahmed <rayhanahmed641@gmail.com>
2024-10-20 20:25:02 +0600

    Scope code + theory

---
+++ 1. Scope/script.js
@@ -0,0 +1,33 @@
+// //1.
+// function showName() {
+//     let name1 = "GeeksforGeeks";
+// }
+
+// showName();
+// console.log(name1);
+//-----------------------------------------------------
+//2
+let name2;
+function showName() {
+    name2 = "GeeksforGeeks";
+}
+
+showName();
+console.log(name2);
+
+//----------------------------------------------------
+//3
+function showName1() {
+    let name = "GeeksforGeeks";
+    console.log(name);
+}
+
+showName1();
+//-----------------------------------------------------
+//4
+function fun() {
+    let a = 10;
+    console.log(a);
+}
+console.log(a);
+
