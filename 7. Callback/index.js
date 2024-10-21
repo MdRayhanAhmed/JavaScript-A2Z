9c69562e25c28f13468fceba74974745e6a6d48e
Md Rayhan Ahmed <rayhanahmed641@gmail.com>
2024-10-21 20:54:17 +0600

    rename callback

--- 7. Callback/index.js
+++ 7. Callback/index.js
@@ -64,6 +64,15 @@ function callbackFunction(number) {
 }
 // Call the main function with the callback function
 mainFunction(callbackFunction);
+//--------------------------------------------------------------
+const greeting = (name) => {
+    console.log('Hello  ' + name);
+}
 
+const processUserName = (callback) => {
+    name = 'GeeksforGeeks';
+    callback(name);
+}
+processUserName(greeting);
 
 
