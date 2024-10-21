29a73f4ec1e48a9b5e25ee22cb124bc1c49332e0
Md Rayhan Ahmed <rayhanahmed641@gmail.com>
2024-10-21 20:55:02 +0600

    promises

---
+++ 2. Promises/script.js
@@ -0,0 +1,22 @@
+//1.
+
+const promise = new Promise((resolve, reject) => {
+    isNameExist = true;
+    if (isNameExist) {
+        resolve("User name exist")
+    } else {
+        reject("error")
+    }
+})
+promise.then(result => console.log(result)).catch(() => { console.log('error!'); });
+
+
+//-----------------------------------------------------
+//2
+
+
+//----------------------------------------------------
+//3
+
+
+
