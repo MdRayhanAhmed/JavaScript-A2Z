d6312185775a22485a983c45b7c8c37474fe6bfd
Md Rayhan Ahmed <rayhanahmed641@gmail.com>
2024-10-21 20:56:02 +0600

    Hoisting

---
+++ 5. Hoisting/script.js
@@ -0,0 +1,21 @@
+//1.
+cowSays('moo');
+
+function cowSays(sound) {
+    console.log(sound);
+}
+
+
+//-----------------------------------------------------
+//2
+
+myfunction(45);
+function myfunction(params) {
+    console.log(params)
+}
+myfunction(45);
+//----------------------------------------------------
+//3
+
+
+
