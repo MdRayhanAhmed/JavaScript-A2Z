9a56d4b1318d6f684e200132245350b577dccf1d
Md Rayhan Ahmed <rayhanahmed641@gmail.com>
2024-10-23 23:36:40 +0600

    solve day 7 Problem 21,22

--- 10 Days of Javascript/Day 07/script.js
+++ 10 Days of Javascript/Day 07/script.js
@@ -12,3 +12,32 @@ function regexVar() {
      */
     return re;
 }
+
+//Problem 21 - Day 7: Regular Expressions II
+function regexVar() {
+    /*
+     * Declare a RegExp object variable named 're'
+     * It must match a string that starts with 'Mr.', 'Mrs.', 'Ms.', 'Dr.', or 'Er.', 
+     * followed by one or more letters.
+     */
+
+    const re = /^(Mr\.|Mrs\.|Ms\.|Dr\.|Er\.)[a-zA-Z ]+$/;
+    /*
+     * Do not remove the return statement
+     */
+    return re;
+}
+
+//Problem 23 - Day 7: Regular Expressions III
+function regexVar() {
+    /*
+     * Declare a RegExp object variable named 're'
+     * It must match ALL occurrences of numbers in a string.
+     */
+    const re = /([0-9])+/g;
+
+    /*
+     * Do not remove the return statement
+     */
+    return re;
+}
