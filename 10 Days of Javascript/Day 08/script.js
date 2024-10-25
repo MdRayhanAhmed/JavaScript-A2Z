e304bb9a0c23c951d12a4bc28b9d459b57f74153
Md Rayhan Ahmed <rayhanahmed641@gmail.com>
2024-10-25 23:50:38 +0600

    solution of button

--- 10 Days of Javascript/Day 08/script.js
+++ 10 Days of Javascript/Day 08/script.js
@@ -1,4 +1,17 @@
 // Problem 23 - Day 8:  Create a Button
+var btn = document.createElement("Button");
 
+btn.innerHTML = "0";
+btn.id = "btn";
+btn.className = "btnClass";
+
+document.body.appendChild(btn);
+
+btn.onclick = function () {
+    btn.innerHTML++;
+}
 
 // Problem 24 - Day 8: Buttons Container
+
+
+
