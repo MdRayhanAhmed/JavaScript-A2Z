70f94e67aac6f343b6bf9df9cb146fcf426665e9
Md Rayhan Ahmed <rayhanahmed641@gmail.com>
2024-10-29 23:03:34 +0600

    Update script.js

--- 30 Days of JavaScript/08. Function Composition/script.js
+++ 30 Days of JavaScript/08. Function Composition/script.js
@@ -12,3 +12,35 @@ var compose = function (functions) {
         return input;
     };
 };
+
+
+//-------------------------------------------------------
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+// Compose function that takes an array of functions
+var compose = function (functions) {
+    // Return a new function
+    return function (x) {
+        // Start with the initial value of x
+        let result = x;
+
+        // Iterate over the functions array from right to left
+        for (let i = functions.length - 1; i >= 0; i--) {
+            // Apply each function to the current result
+            result = functions[i](result);
+        }
+
+        // Return the final result after applying all functions
+        return result;
+    };
+};
+
+
+
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
