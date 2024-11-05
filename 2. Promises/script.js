aae9d763e03e17527676b4fec8274aa0ff56c8f2
Md Rayhan Ahmed <rayhanahmed641@gmail.com>
2024-11-06 03:08:01 +0600

    Update Promises file

--- 2. Promises/script.js
+++ 2. Promises/script.js
@@ -3,24 +3,24 @@
 const promise = new Promise((resolve, reject) => {
     isNameExist = true;
     if (isNameExist) {
-        resolve("User name exist")
+        resolve("User name exist00")
     } else {
-        reject("error")
+        reject("error00")
     }
 })
-promise.then(result => console.log(result)).catch(() => { console.log('error!'); });
+promise.then(result => console.log(result)).catch(() => { console.log('error00!'); });
 
 
 //-----------------------------------------------------
 //2
 const promiseOne = new Promise(function (resolve, reject) {
     setTimeout(function () {
-        console.log('Async task is complete');
+        console.log('Async task is complete01');
         resolve();
     }, 1000)
 })
 promiseOne.then(function () {
-    console.log('Promise consumed');
+    console.log('Promise consumed01');
 
 })
 
@@ -28,11 +28,11 @@ promiseOne.then(function () {
 //3
 new Promise(function (resolve, reject) {
     setTimeout(function () {
-        console.log('Async task 2 is complete')
+        console.log('Async task 2 is complete02')
         resolve()
     }, 1000)
 }).then(function () {
-    console.log("Async 2 resolved");
+    console.log("Async 2 resolved02");
 })
 //----------------------------------------------------
 //4
@@ -54,7 +54,7 @@ const promiseFour = new Promise((resolve, reject) => {
             resolve({ username: "rayhan", password: "12345" })
 
         } else {
-            reject("Error: Something went wrong")
+            reject("Error: Something went wrong04")
         }
     }, 1000);
 })
@@ -68,30 +68,83 @@ promiseFour.then((user) => {
     console.log(error);
 
 }).finally(() => {
-    console.log("The Promise is either resolve or reject");
+    console.log("The Promise is either resolve or reject04");
 
 })
 
 //----------------------------------------------------
 //6
-const promiseFive = new Promise(function (resolve, reject) {
-    setTimeout(function () {
-        let error = true
-        if (!error) {
-            resolve({ username: "javascript", password: "123" })
-        } else {
-            reject('ERROR: JS went wrong')
-        }
-    }, 1000)
-});
+// const promiseFive = new Promise(function (resolve, reject) {
+//     setTimeout(function () {
+//         let error = true
+//         if (!error) {
+//             resolve({ username: "javascript", password: "123" })
+//         } else {
+//             reject('ERROR: JS went wrong')
+//         }
+//     }, 1000)
+// });
+
+// async function consumePromiseFive() {
+//     try {
+//         const response = await promiseFive
+//         console.log(response);
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+
+// consumePromiseFive()
+
+
+
+const promiseFiveTest = new Promise((resolve, reject) => {
+    let error = false;
+    if (!error) {
+        resolve({ username: "Rayhan", password: "1234567" })
+    }
+    else {
+        reject("ERRor: Somthing is wrong05")
+    }
+})
 
 async function consumePromiseFive() {
     try {
-        const response = await promiseFive
+        const response = await promiseFiveTest
         console.log(response);
-    } catch (error) {
+    }
+    catch (error) {
         console.log(error);
+    }
+
+}
+consumePromiseFive()
+//------------------------------------------------------
+//7.
+
+async function getAllUsers() {
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+
+        const data = await response.json()
+        console.log(data);
+    } catch (error) {
+        console.log("E: ", error);
     }
 }
 
-consumePromiseFive()
+getAllUsers()
+
+// fetch('https://api.github.com/users/hiteshchoudhary')
+
+fetch('https://api.github.com/users/MdRayhanAhmed')
+    .then((response) => {
+        return response.json()
+    })
+    .then((data) => {
+        console.log(data);
+    })
+    .catch((error) => console.log(error))
+
+// promise.all
+// yes this is also available, kuch reading aap b kro.
